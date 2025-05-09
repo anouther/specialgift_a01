228c40cd3335b1a1f350909f47947007d945e09f
anouther <155570107+anouther@users.noreply.github.com>
2025-05-09 21:53:37 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('hille, i love u.').split('');
+        const titles = ('I LOVE YOU EILS').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "I’m proud of you, thankful for everything you’ve done, and I love you so much. Always by your side.";
+            subtitle.innerText = "thank u for being in my life, i lovee u so so sooo much (Zefdran)";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
